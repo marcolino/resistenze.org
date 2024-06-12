a7651a40cdcccb5c73af603d41530fde5635f9a5
marco <marco@solari@gmail.com>
2024-06-12 21:48:11 +0200

    Testing CI

---
+++ tools/enableip.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# Enable current IP
+ 
+import requests
+from bs4 import BeautifulSoup
+
+login_page = 'https://admin.aruba.it/PannelloAdmin/Login.aspx'
+username = '236291@aruba.it'
+password = '!Pisa340.24'
+
+# step 1: fetch the login page
+session = requests.Session()
+response = session.get(login_page)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# step 2: extract hidden fields
+hidden_fields = {}
+hidden_tags = soup.find_all("input", type = "hidden")
+for tag in hidden_tags:
+  name = tag.get('name', '')
+  value = tag.get('value', '')
+  hidden_fields[name] = value
+print('hidden fields:', hidden_fields)
+
+# step 3: prepare login data including hidden fields
+login_data = {
+  'login': username,
+  'password': password,
+  **hidden_fields
+}
+print ("----------------------")
+print('login_data:', login_data)
+
+# step 4: submit the login form
+login_url = login_page
+login_response = session.post(login_url, data = login_data)
+
+# check if login was successful
+print(login_response.text)
---
+++ tools/enableip.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Enable current IP
+
+PAGE_LOGIN="https://admin.aruba.it/PannelloAdmin/Login.aspx"
+PAGE_DOMAIN_CHOOSE="https://admin.aruba.it/PannelloAdmin/LoginDomain.aspx?Lang=IT"
+USERNAME='236291@aruba.it'
+PASSWORD='!Pisa340.24'
+
+# login
+# curl \
+#   -s -o /dev/null \
+#   -X GET \
+#   -d "username=${USERNAME}&password=${PASSWORD}" \
+#   -H "Content-Type: application/x-www-form-urlencoded" \
+#   -c "cookies.txt" \
+#   "${PAGE_LOGIN}"
+curl \
+  -s \
+  -X GET \
+  --form login="$USERNAME" \
+  --form password="$PASSWORD" \
+  --cookie-jar cookies.txt \
+  "${PAGE_LOGIN}"
+exit
+
+# choose domain
+curl \
+  -s \
+  -X GET \
+  -b "cookies.txt" \
+  "${PAGE_DOMAIN_CHOOSE}"
+
